c14213b3cf8826d0bba5e71cb82136fd5ab3d5b5
Tom Schneider <tom.schneider@simpleitsolutions.ch>
2019-07-31 09:43:24 +0200

    FIX: Added check on Passenger before processing payment report.

--- src/Helper/PaymentViewHelper.php
+++ src/Helper/PaymentViewHelper.php
@@ -19,39 +19,45 @@ class PaymentViewHelper
         {
             $transactionNo = $payment->getTransactionNo();
             $passenger = $payment->getPurchases()[0]->getPassenger();
-            $pilot = $passenger->getPilot();
-            $booking = $passenger->getBooking();
-            if(!array_key_exists ( $transactionNo, $this->paymentViewList ))
-            {
-                $paymentView = new BookingPaymentView();
-                $paymentView->setPaymentDateTime($payment->getCreated());
-                $paymentView->setTransactionNo($transactionNo);
-                $paymentView->setPaymentAmount($payment->getAmount());
-                $paymentView->setNotes($payment->getDescription());
-                $paymentView->setSumUpRef($payment->getSumupRef());
-                $paymentView->addPassenger($passenger);
-                if($payment->getSubAmount() <> 0.0)
+
+            if(isset($passenger)) {
+
+//                if(is_null($passenger)) {throw new \Exception("HERE ".$payment->getPurchases()[0]->getId());}
+                $pilot = $passenger->getPilot();
+                $booking = $passenger->getBooking();
+                if(!array_key_exists ( $transactionNo, $this->paymentViewList ))
                 {
-//                     $paymentView->addSubPaymentAmount($payment->getSubAmount());
+                    $paymentView = new BookingPaymentView();
+                    $paymentView->setPaymentDateTime($payment->getCreated());
+                    $paymentView->setTransactionNo($transactionNo);
+                    $paymentView->setPaymentAmount($payment->getAmount());
+                    $paymentView->setNotes($payment->getDescription());
+                    $paymentView->setSumUpRef($payment->getSumupRef());
+                    $paymentView->addPassenger($passenger);
+                    if($payment->getSubAmount() <> 0.0)
+                    {
+    //                     $paymentView->addSubPaymentAmount($payment->getSubAmount());
+                    }
+                    $paymentView->addPilot($pilot);
+                    $paymentView->setBookingNo($booking->getId()) ;
+                    $paymentView->setFlightDate($booking->getFlightDate());
+                    $paymentView->setMeetingTime($booking->getMeetingTime());
+                    $paymentView->setRefunded($payment->getRefunded());
+                    $paymentView->setRefundedDate($payment->getUpdated());
+                    $paymentView->setPaymentType($payment->getPaymentType());
+
+                    $this->paymentViewList[$transactionNo] = $paymentView;
                 }
-                $paymentView->addPilot($pilot);
-                $paymentView->setBookingNo($booking->getId()) ;
-                $paymentView->setFlightDate($booking->getFlightDate());
-                $paymentView->setMeetingTime($booking->getMeetingTime());
-                $paymentView->setRefunded($payment->getRefunded());
-                $paymentView->setRefundedDate($payment->getUpdated());
-                $paymentView->setPaymentType($payment->getPaymentType());
-    
-                $this->paymentViewList[$transactionNo] = $paymentView;
+                else
+                {
+                    $paymentView = $this->paymentViewList[$transactionNo];
+
+                    $paymentView->addPassenger($passenger);
+    //                 $paymentView->addSubPaymentAmount($payment->getSubAmount());
+                    $paymentView->addPilot($pilot);
+                }
             }
-            else
-            {
-                $paymentView = $this->paymentViewList[$transactionNo];
-    
-                $paymentView->addPassenger($passenger);
-//                 $paymentView->addSubPaymentAmount($payment->getSubAmount());
-                $paymentView->addPilot($pilot);
-            }
+
         }
         return $this->paymentViewList;
     }
