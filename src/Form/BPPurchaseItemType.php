d8f725ac05b7c5c2caf3437204b5fa5e37edfbe8
Tom Schneider <tom.schneider@simpleitsolutions.ch>
2019-07-31 09:41:22 +0200

    - Modified queries used to populate product(flight) field;

--- src/Form/BPPurchaseItemType.php
+++ src/Form/BPPurchaseItemType.php
@@ -3,6 +3,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\PurchaseItem;
+use App\Repository\ProductRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -13,10 +14,40 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class BPPurchaseItemType extends AbstractType
 {
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
     	// $builder->add('product', 'entity', array('class' => 'AazpBookingBundle:Product', 'property' => 'name', 'empty_value' => 'Please Select', 'empty_data' => null, 'required' => false));
-        $builder->add('product', EntityType::class, array('label' => 'Flight', 'class' => Product::class, 'choice_label' => 'description', 'placeholder' => 'Please Select', 'required' => false));
+        $builder->add('product',
+                    EntityType::class,
+                            ['label' => 'Flight',
+                                'class' => Product::class,
+                                'choice_label' => 'description',
+                                'placeholder' => 'Please Select',
+                                'required' => false,
+                                'empty_data' => null,
+                                'preferred_choices' => $this->productRepository->getPreferredFlightProducts(),
+                                'group_by' => function($choice, $key, $value) {
+                                    if ($choice->getProductCategory() == "FLIGHT") {
+                                        return 'Flight';
+                                    } else if($choice->getProductCategory() == "TRANSPORT") {
+                                        return 'Transport';
+                                    } else {
+                                        return 'Other';
+                                    }
+                                },
+                                'query_builder' => function (ProductRepository $er) {
+                                    return $er->createQueryBuilder('p')
+                                        ->join('p.productCategory', 'pc')
+                                        ->where('pc.name NOT IN(:categories)')
+                                        ->setParameter('categories', ['MISC','VOUCHER'])
+                                        ->orderBy('p.sortOrder', 'ASC');
+                                }
+                            ]);
 		// $builder->add('description');
     	$builder->add('amount', NumberType::class, array('scale' => 2));
     }
